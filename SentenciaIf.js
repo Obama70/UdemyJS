5efa0867b02dcdc034cbcc49ec21d40c68cfabd2
sebastian <cayisebas99@gmail.com>
2024-11-20 15:42:32 -0600

    sentencia if

---
+++ SentenciaIf.js
@@ -0,0 +1,10 @@
+//sentencia if
+//comprobar si un valor es positivo 
+let a = 10;
+
+if(a > 0){
+    console.log(`${a} es un valor positivo`)
+}
+else{
+    console.log(`${a} es un valor negativo`)
+}
