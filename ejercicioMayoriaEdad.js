e89a2994be34f8f8ef76050244f2686057e182a5
sebastian <cayisebas99@gmail.com>
2024-11-20 16:04:36 -0600

    ejercicio mayoria de edad

---
+++ ejercicioMayoriaEdad.js
@@ -0,0 +1,14 @@
+//ejercicio mayor de edad
+
+let miEdad = 18;
+
+if(miEdad > 18){
+    console.log(`eres mayor de edad tiens ${miEdad} años`)
+}
+else if(miEdad < 18){
+    console.log(`eres menor de edad tienes ${miEdad} años`)
+
+}
+else{
+    console.log(`tienes exactamente ${miEdad} años, eres mayor de edad`)
+}
