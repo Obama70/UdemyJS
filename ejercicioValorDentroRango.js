7175806e8f8251acca1e7328f9196b0528a0a74d
sebastian <cayisebas99@gmail.com>
2024-11-20 15:26:29 -0600

    ejercicio valor dentro rango

---
+++ ejercicioValorDentroRango.js
@@ -0,0 +1,8 @@
+//operadores logicos 
+//ejercicio valor dentro de rango 
+
+let ranmin = 10, ranmax = 20;
+
+let valRan = 11;
+
+console.log(`valor dentro del rango ${valRan>ranmin && valRan<ranmax}`)
