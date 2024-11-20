193bd39a3165d831b635c3b8d6de56cb9e2878c5
sebastian <cayisebas99@gmail.com>
2024-11-20 16:12:11 -0600

    ejercicio dia de la semana Switch

---
+++ ejercicioDiaDeLaSemanaSwitch.js
@@ -0,0 +1,30 @@
+//dia de la semana switch
+
+let dia = 8;
+
+switch(dia){
+    case 1:
+        console.log("lunes")
+        break
+    case 2:
+        console.log("martes")
+        break
+    case 3:
+        console.log("miercoles")
+        break
+    case 4:
+        console.log("jueves")
+        break
+    case 5:
+        console.log("viernes")
+        break
+    case 6:
+        console.log("sabado")
+        break
+    case 7:
+        console.log("domingo")
+        break
+    default:
+        console.log("dia introducido erroneo")
+        break
+}//final switch
