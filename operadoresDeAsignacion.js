7d00e6647321038e932fb81f32d1b7bcec0e5c0d
sebastian <cayisebas99@gmail.com>
2024-11-20 14:43:14 -0600

    operadores de asignacion

---
+++ operadoresDeAsignacion.js
@@ -0,0 +1,32 @@
+//operadores de asignacion 
+
+let miNumero =10;
+console.log(miNumero);
+
+miNumero = 15;
+console.log(miNumero)
+
+//operador asignacion compuesto 
+miNumero += 5;
+console.log(miNumero)
+
+//operador -=
+miNumero -=3;
+console.log(miNumero)
+
+// operador *=
+miNumero *= 2;
+console.log(miNumero)
+
+// operador /=
+miNumero /= 4;
+console.log(miNumero)
+
+//operador %=
+miNumero %= 2;
+console.log(miNumero)
+
+//exponente **=
+
+miNumero **= 2;
+console.log(miNumero)
