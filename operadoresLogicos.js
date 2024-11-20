786138fe98063eaae3898f968cae8da3b8fe0bfe
sebastian <cayisebas99@gmail.com>
2024-11-20 15:19:43 -0600

    operadores logicos

---
+++ operadoresLogicos.js
@@ -0,0 +1,18 @@
+//operadores logicos
+//&&(and), ||(or), !(not).
+let a = true;
+let b = false;
+console.log(a);
+console.log(b);
+
+//operador logico and(&&)
+//regresa verdadero si ambos operandos son verdaderos 
+console.log(`${a} && ${b} -> ${a&&b}`)
+
+//operador logico or(||)
+//regresa verdadero si cualquiera de los dos operandos son verdaderos
+console.log(`${a} || ${b} -> ${a||b}`)
+
+//operador logico not(!)
+//convierte cualquier operando a su contrario notTrue notFalse
+console.log(`${a} ! ${!a}, ${b} ! ${!b}`)
