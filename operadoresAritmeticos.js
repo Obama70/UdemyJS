6d1e45ed3f84ca3c66fdf593e32ae279ea040561
sebastian <cayisebas99@gmail.com>
2024-11-20 14:25:26 -0600

    operadores aritmeticos

---
+++ operadoresAritmeticos.js
@@ -0,0 +1,32 @@
+//operadores aritmeticos
+let a,b,c,d,e,f;
+
+//suma 
+a=3+4;
+console.log(a)
+
+//resta
+b = 6-2;
+console.log(b)
+
+//multiplicacion
+c= a*2;
+console.log(c)
+
+//division
+d = b/2.5;
+console.log(d)
+console.log(typeof d)
+
+//modulo o residuo 
+f= 9%2
+console.log(f)
+
+
+//potencia 
+e = 2**3;
+console.log(e)
+//2*2*2 = 8
+
+e= Math.pow(2,3);
+console.log(e)
