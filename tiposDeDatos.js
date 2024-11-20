c98e50d6de875d75f3662df2b153a1ae8f76b881
sebastian <cayisebas99@gmail.com>
2024-11-20 13:39:03 -0600

    tipos de datos

---
+++ tiposDeDatos.js
@@ -0,0 +1,21 @@
+//tipos de datos 
+let miEntero = 10;
+console.log(miEntero)
+
+miEntero="hola"
+console.log(miEntero)
+
+let miFlotante=10.5;
+console.log(miFlotante)
+
+let miCadena="hola"
+console.log(miCadena)
+
+let miBoolean=true;
+console.log(miBoolean) 
+
+let miNull= null;
+console.log(miNull)
+
+let miUndefined=undefined;
+console.log(undefined)
