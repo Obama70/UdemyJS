6404dfeec6ec348f52df9e37940da50c7330c3b2
sebastian <cayisebas99@gmail.com>
2024-11-20 16:07:51 -0600

    ejercicio dia de la semana

---
+++ ejercicioDiaDeLaSemana.js
@@ -0,0 +1,28 @@
+//dia de la semana 
+
+let dia = 7;
+
+if(dia == 1){
+    console.log("lunes")
+}
+else if(dia == 2){
+    console.log("martes")
+}
+else if(dia == 3){
+    console.log("miercoles")
+}
+else if(dia == 4){
+    console.log("jueves")
+}
+else if(dia == 5){
+    console.log("viernes")
+}
+else if(dia == 6){
+    console.log("sabado")
+}
+else if(dia == 7){
+    console.log("domingo")
+}
+else{
+    console.log("dia de la semana erroneo")
+}
