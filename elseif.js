40e6e285b9f0672a291963cf80b845e574580ccb
sebastian <cayisebas99@gmail.com>
2024-11-20 15:51:57 -0600

    else if

---
+++ elseif.js
@@ -0,0 +1,12 @@
+//elseif
+let a = 0;
+
+if(a > 0){
+    console.log(`${a} es un valor positivo`)
+}
+else if(a < 0){
+    console.log(`${a} valor introducido es negativo`)
+}
+else{
+    console.log(`${a} es cero`)
+}
