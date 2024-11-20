e5ad5b925af1529dd8eafab56a3f23e421b5e6e5
sebastian <cayisebas99@gmail.com>
2024-11-20 15:58:43 -0600

    operador ternario

---
+++ operadorTernario.js
@@ -0,0 +1,13 @@
+//operador ternario
+
+let a = 10;
+/*
+if(a > 0){
+    console.log(`${a} es un valor positivo`)
+}
+else{
+    console.log(`${a} es un valor negativo`)
+}
+*/
+
+(a > 0) ? console.log(`positivo ${a}`): console.log(`negativo ${a}`);
