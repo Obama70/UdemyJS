10ddf3c19a144bf2100c673b10b63543f3103b2d
sebastian <cayisebas99@gmail.com>
2024-11-20 13:56:19 -0600

    constantes

---
+++ constantes.js
@@ -0,0 +1,11 @@
+const MI_CONSTANTE = 150;
+console.log(MI_CONSTANTE)
+//MI_CONSTANTE = 200 no se puede cambiar el valor de una constante 
+console.log(MI_CONSTANTE)
+
+const SEGUNDOS_POR_MINUTO=60;
+console.log(SEGUNDOS_POR_MINUTO)
+
+console.log(Math.PI)
+
+//con las constantes no se puede hacer hoisting 
--- tiposDeDatos.js
+++ tiposDeDatos.js
@@ -1,21 +1,28 @@
 //tipos de datos 
 let miEntero = 10;
 console.log(miEntero)
+console.log(typeof miEntero);
 
 miEntero="hola"
 console.log(miEntero)
+console.log(typeof miEntero)
 
 let miFlotante=10.5;
 console.log(miFlotante)
+console.log(typeof miFlotante)
 
 let miCadena="hola"
 console.log(miCadena)
+console.log(typeof miCadena)
 
 let miBoolean=true;
 console.log(miBoolean) 
+console.log(typeof miBoolean)
 
 let miNull= null;
 console.log(miNull)
+console.log(typeof miNull)
 
 let miUndefined=undefined;
 console.log(undefined)
+console.log(typeof miUndefined)
