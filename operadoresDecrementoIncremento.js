45ccd00f23b69ed7c575951601c0a198e00dccf7
sebastian <cayisebas99@gmail.com>
2024-11-20 14:35:39 -0600

    Incremento Decremento

---
+++ operadoresDecrementoIncremento.js
@@ -0,0 +1,22 @@
+// operadores incremento decremento 
+let a = 0;
+
+//preincremento
+++a;
+console.log(a)
+//postincremento
+a++;
+console.log(a)
+
+//postdecremento
+a--;
+console.log(a)
+//predecremento
+--a;
+console.log(a);
+
+//ejemplo 
+a=5
+b=6
+c=++a * b--;
+console.log(c)
