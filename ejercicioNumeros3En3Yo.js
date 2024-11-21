d4a6a4ac888ff49b1febc50310fbc48c2ac49ff5
sebastian <cayisebas99@gmail.com>
2024-11-21 14:47:45 -0600

    ejercicio suma acumulativa

---
+++ ejercicioNumeros3En3Yo.js
@@ -0,0 +1,11 @@
+//imprimir numeros de 3 en 3 
+
+let a = 0;
+
+let c = 60;
+
+for(a;a<=c;a++){
+    if(a%3==0){
+        console.log(a)
+    }
+}
---
+++ ejercicioSumaPrimeros5Numeros.js
@@ -0,0 +1,11 @@
+//imprimir suma de los primeros 5 numeros
+
+
+let a = 0, b=5, sumador=0;
+
+for(a;a<=b;a++){
+    console.log(a)
+    sumador+=a
+    console.log(sumador)
+
+}
