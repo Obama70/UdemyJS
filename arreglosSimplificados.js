188112760a3755524d791d4cb87c5989fe53c0a0
sebastian <cayisebas99@gmail.com>
2024-11-21 15:48:57 -0600

    arreglos simplificados

---
+++ arreglosSimplificados.js
@@ -0,0 +1,13 @@
+//areglos simplificados 
+
+
+let numerosArray=[100,200,5,3,2];
+
+console.log(numerosArray[0]);
+console.log(numerosArray[2]);
+console.log(numerosArray[3]);
+console.log(numerosArray[4]);
+
+for(let i = 0; i< numerosArray.length; i++){
+    console.log(numerosArray[i])
+}
