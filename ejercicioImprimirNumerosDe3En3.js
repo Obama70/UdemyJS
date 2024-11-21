87b3a84108698a5049b2bbfee513b6d8bf86117b
sebastian <cayisebas99@gmail.com>
2024-11-21 14:36:43 -0600

    ejercicio numeros 3 en 3

---
+++ ejercicioImprimirNumerosDe3En3.js
@@ -0,0 +1,15 @@
+//ciclo imprimir de 3 en 3
+
+let a = 0;
+
+for(let i = 0; i <= 15; i+=3){
+    console.log(i)
+}
+
+while(a<15){
+    console.log(a+=3)
+}
+
+do {
+    console.log(a+=3);
+} while (a<15);
