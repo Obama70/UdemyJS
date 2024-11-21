25a9d0fc3fe5502ef064c8977cbf35f1c5496115
sebastian <cayisebas99@gmail.com>
2024-11-21 15:35:13 -0600

    arreglos

---
+++ arreglos.js
@@ -0,0 +1,16 @@
+//arreglos 
+
+let numerosArreglo=[5];
+
+numerosArreglo[0]=5;
+numerosArreglo[1]="hola";
+numerosArreglo[4]=123;
+numerosArreglo[5]=555
+
+
+console.log(numerosArreglo[0]);
+console.log(numerosArreglo[1]);
+console.log(numerosArreglo[2]);
+console.log(numerosArreglo[3]);
+console.log(numerosArreglo[4]);
+console.log(numerosArreglo[5]);
