a65c33553cf9499d96eb86a48144180ec61c707a
sebastian <cayisebas99@gmail.com>
2024-11-22 13:27:34 -0600

    declaracion funcion

---
+++ declaracionFuncion.js
@@ -0,0 +1,18 @@
+//declarar funcion funcion
+
+function suma(a,b){
+    return a+b;
+}
+
+let a=55,b=9;
+
+//llamamos a la funcion y le pasamos argumentos
+//existen 2 maneras de guardar el return de una funcion
+//en una variable 
+
+let result= suma(a,b);
+console.log(result)
+
+//o directamente en un console.log
+console.log(suma(a,b))
+
