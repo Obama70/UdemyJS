c264270bd7e5086acd2ff99a09c99a4f200eef32
sebastian <cayisebas99@gmail.com>
2024-11-23 12:04:07 -0600

    funcion incorporada PARSEINT string to number

---
+++ FuncionIncorporadaParseint.js
@@ -0,0 +1,9 @@
+//convertir cadenas a numeros
+let a = "10", b = "20";
+//si son cadenas se concatenan
+console.log(a+b);
+
+//convertir a numeros para que se sumen
+//SE HACE CON PARSEINT
+let res = parseInt(a)+parseInt(b);
+console.log(res)
