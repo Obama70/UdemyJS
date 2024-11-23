f01bad27d4cfea117e77c42a07fe44ec6df4ef52
sebastian <cayisebas99@gmail.com>
2024-11-23 12:15:04 -0600

    convertir numero a cadena ToString

---
+++ numeroAcadenaTostring.js
@@ -0,0 +1,12 @@
+//convertir numero a cadena 
+
+let a=10, b=20;
+
+// como son valores se suman 
+console.log(a+b);
+
+//convertirlos valores a cadena 
+
+let restring = a.toString()+b.toString();
+console.log(restring);
+
