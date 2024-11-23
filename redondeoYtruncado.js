8ffb79802734e87968256ba077d0c5ad7fb05bd8
sebastian <cayisebas99@gmail.com>
2024-11-23 12:27:49 -0600

    redondeo y truncaodo

---
+++ redondeoYtruncado.js
@@ -0,0 +1,13 @@
+//redondeo y truncado en js 
+let numero = 8.5, redondeo, truncado;
+
+//redondeo 
+//math.round redondea al valor entero mas cercano 
+
+redondeo = Math.round(numero);
+console.log(redondeo);
+
+//truncado 
+//math.trunc() elimina la parte flotante (decimal)
+truncado= Math.trunc(numero)
+console.log(truncado);
