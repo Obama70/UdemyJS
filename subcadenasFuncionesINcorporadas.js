1ed6503acc3dfc05a44ca0edc865eb5b532a1c06
sebastian <cayisebas99@gmail.com>
2024-11-23 11:36:44 -0600

    substrin en js

---
+++ subcadenasFuncionesINcorporadas.js
@@ -0,0 +1,9 @@
+//subcadena en js 
+let cadena3="hola mundo"
+
+//substring(indiceInicio, indiceFin)
+let cadena1= cadena3.substring(0,4);
+console.log(cadena1)
+
+let cadena2 = cadena3.substring(5,10);
+console.log(cadena2)
