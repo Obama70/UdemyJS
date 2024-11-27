8d74c0a34d716d853b291a5ac4a96382faeb266c
sebastian <cayisebas99@gmail.com>
2024-11-27 08:14:01 -0600

    0801 objetos ujs

---
+++ leccion 8/0801-objects-ujs.js
@@ -0,0 +1,18 @@
+let x= 10;
+console.log(x.length);
+
+let persona ={
+    nombre: 'saul',
+    apellido: 'alvarez',
+    email: 'salvarez@mail.com',
+    edad: 33
+
+}
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.edad);
+
+console.log(persona);
+
+//aqui metiste atributos al objeto persona 
