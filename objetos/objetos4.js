a6b17a8fd7c0c663c4a3832ec032d1c9b83a1215
sebastian <cayisebas99@gmail.com>
2024-12-21 20:22:12 -0600

    metodo set objetos falta mas estudio

---
+++ objetos/objetos4.js
@@ -0,0 +1,29 @@
+let x=10;
+console.log(x.lenght);
+
+
+let persona = {
+    nombre: 'juan',
+    apellido: 'barragan',
+    edad: '24',
+    mail: 'jbarragan@mail.com',
+    idioma: 'es',
+    get lang(){
+        return this.idioma.toUpperCase();
+    },
+    set lang(lang){
+        this.idioma = lang.toUpperCase();
+
+    },
+    get nombreCompleto(){
+        return this.nombre+" "+this.apellido;
+    }
+}
+
+console.log(persona.nombreCompleto);
+console.log(persona.lang);
+
+persona.lang="en";
+console.log(persona.lang);
+
+
