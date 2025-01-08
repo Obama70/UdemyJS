c2c4485592d38ee9c56a58f58192e3ec1fd82826
sebastian <cayisebas99@gmail.com>
2025-01-08 17:11:33 -0600

    clases

---
+++ clases/clases1.js
@@ -0,0 +1,15 @@
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+
+    }
+}
+
+let persona1 = new Persona("juan", "perez");
+
+console.log(persona1);
+
+let persona2 = new Persona("saul", "alvarez");
+console.log(persona2)
+
